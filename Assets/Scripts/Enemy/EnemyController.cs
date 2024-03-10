b5fe935e6fbe0e81de9c7ecfc5db94a83a6a69d1
Egor <129103195+egor-4080@users.noreply.github.com>
2024-03-10 18:56:03 +0300

    AddEnemyRotation&Bullet

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -6,6 +6,10 @@ public class EnemyController : Character
     private Transform player;
     private NavMeshAgent agent;
 
+    private float startScaleX;
+    private float startScaleY;
+    private int setScale;
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -18,11 +22,23 @@ public class EnemyController : Character
 
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        startScaleX = transform.localScale.x;
+        startScaleY = transform.localScale.y;
     }
 
     private void Update()
     {
-        if(player != null)
+        if(transform.position.x - player.position.x > 0)
+        {
+            setScale = -1;
+        }
+        else
+        {
+            setScale = 1;
+        }
+        transform.localScale = new Vector3(setScale * startScaleX, startScaleY, 1);
+
+        if (player != null)
         {
             agent.SetDestination(player.position);
         }
